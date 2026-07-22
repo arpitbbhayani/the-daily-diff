c4325e6f3d43d2f7616ccd3a3a321cbe7a3ae7e0
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2026-07-22 19:13:17 +0000

    Update domain to tdd.cat and increase masthead title size
    - Update site and sitemap domain to tdd.cat in Astro config and robots.txt
    - Update RSS site fallback domain to tdd.cat
    - Add image prop and detailed social metatags to layout
    - Increase font weight and size of masthead name in styles
    - Tighten letter spacing of masthead name in styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://tdd.arpitbhayani.me',
+  site: 'https://tdd.cat',
   integrations: [
     sitemap({
       serialize(item) {
